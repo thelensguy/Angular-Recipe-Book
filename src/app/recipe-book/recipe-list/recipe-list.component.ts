4568e9050485d24e279cd66a5cf6ab160890c0f9
Than Nguyenvu <thelensguy@gmail.com>
2020-06-05 14:39:32 -0700

    Set up sekeleton to display recipe details

--- src/app/recipe-book/recipe-list/recipe-list.component.ts
+++ src/app/recipe-book/recipe-list/recipe-list.component.ts
@@ -8,7 +8,8 @@ import { RecipeBook } from '../recipe-book.model';
 })
 export class RecipeListComponent implements OnInit {
   recipes: RecipeBook[] = [
-    new RecipeBook('A test recipe', 'This is simply a test', 'https://images.unsplash.com/photo-1490818387583-1baba5e638af?ixlib=rb-1.2.1&w=1000&q=80')
+    new RecipeBook('A test recipe', 'This is simply a test', 'https://images.unsplash.com/photo-1490818387583-1baba5e638af?ixlib=rb-1.2.1&w=1000&q=80'),
+    new RecipeBook('A test recipe', 'This is simply a test', 'https://images.unsplash.com/photo-1490818387583-1baba5e638af?ixlib=rb-1.2.1&w=1000&q=80'),
   ];
 
   constructor() { }
