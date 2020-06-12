6d14c58712b14ccbb3ee63650c3b5ff63a95cdd7
Than Nguyenvu <thelensguy@gmail.com>
2020-06-11 20:55:34 -0700

    added navigational features

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
+  @Output() featureSelected = new EventEmitter<string>();
 
-  constructor() { }
-
-  ngOnInit(): void {
+  onSelect(feature: string) {
+    this.featureSelected.emit(feature);
   }
 
+  constructor() {}
+
+  ngOnInit(): void {}
 }
